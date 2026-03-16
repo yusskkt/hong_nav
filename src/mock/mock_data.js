ec231da49bda424aede3952ce81c87c848e5e7ab
Yu Hongbo <62323127+yusskkt@users.noreply.github.com>
2026-03-16 19:57:35 +0800

    chore: 更新导航数据 - 2026/3/16 19:57:34

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -33,6 +33,41 @@ export const mockData = {
           "url": "https://www.bilibili.com/",
           "description": "",
           "icon": ""
+        },
+        {
+          "id": "site-1753275507949",
+          "name": "新闻",
+          "url": "https://www.bestblogs.dev/",
+          "description": "",
+          "icon": "/sitelogo/www.bestblogs.dev.ico"
+        },
+        {
+          "id": "site-1753275456725",
+          "name": "影视推荐",
+          "url": "https://ayouth.top/ayouth/video.html",
+          "description": "",
+          "icon": "/sitelogo/ayouth.top.ico"
+        },
+        {
+          "id": "youtube",
+          "name": "YouTube",
+          "url": "https://www.youtube.com",
+          "description": "视频分享平台",
+          "icon": "/sitelogo/www.youtube.com.ico"
+        },
+        {
+          "id": "zhihu",
+          "name": "知乎",
+          "url": "https://www.zhihu.com",
+          "description": "知识问答社区",
+          "icon": "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='black'%3E%3Cpath d='M8 0C3.58 0 0 3.58 0 8a8 8 0 005.47 7.59c.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.01.08-2.11 0 0 .67-.21 2.2.82A7.65 7.65 0 018 4.58c.68.003 1.36.092 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.91.08 2.11.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0016 8c0-4.42-3.58-8-8-8z'/%3E%3C/svg%3E"
+        },
+        {
+          "id": "site-1753279757336",
+          "name": "热榜",
+          "url": "https://trend.tgmeng.com/",
+          "description": "",
+          "icon": ""
         }
       ]
     },
@@ -111,15 +146,7 @@ export const mockData = {
           "url": "https://www.pasyun.com/",
           "description": "ipv6机器",
           "icon": "/sitelogo/www.pasyun.com.ico"
-        }
-      ]
-    },
-    {
-      "id": "tools",
-      "name": "在线工具",
-      "icon": "⚙️",
-      "order": 7,
-      "sites": [
+        },
         {
           "id": "site-1753275494462",
           "name": "滴答清单",
@@ -133,63 +160,6 @@ export const mockData = {
           "url": "https://v.flomoapp.com/mine",
           "description": "",
           "icon": "/sitelogo/v.flomoapp.com.ico"
-        }
-      ]
-    },
-    {
-      "id": "entertainment",
-      "name": "娱乐休闲",
-      "icon": "🎮",
-      "order": 8,
-      "sites": [
-        {
-          "id": "bilibili",
-          "name": "哔哩哔哩",
-          "url": "https://www.bilibili.com",
-          "description": "弹幕视频网站",
-          "icon": "/sitelogo/www.bilibili.com.ico"
-        },
-        {
-          "id": "youtube",
-          "name": "YouTube",
-          "url": "https://www.youtube.com",
-          "description": "视频分享平台",
-          "icon": "/sitelogo/www.youtube.com.ico"
-        },
-        {
-          "id": "douban",
-          "name": "豆瓣",
-          "url": "https://www.douban.com",
-          "description": "文艺生活社区",
-          "icon": "/sitelogo/www.douban.com.ico"
-        },
-        {
-          "id": "zhihu",
-          "name": "知乎",
-          "url": "https://www.zhihu.com",
-          "description": "知识问答社区",
-          "icon": "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='black'%3E%3Cpath d='M8 0C3.58 0 0 3.58 0 8a8 8 0 005.47 7.59c.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.01.08-2.11 0 0 .67-.21 2.2.82A7.65 7.65 0 018 4.58c.68.003 1.36.092 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.91.08 2.11.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0016 8c0-4.42-3.58-8-8-8z'/%3E%3C/svg%3E"
-        },
-        {
-          "id": "site-1753275456725",
-          "name": "影视推荐",
-          "url": "https://ayouth.top/ayouth/video.html",
-          "description": "",
-          "icon": "/sitelogo/ayouth.top.ico"
-        },
-        {
-          "id": "site-1753275507949",
-          "name": "新闻",
-          "url": "https://www.bestblogs.dev/",
-          "description": "",
-          "icon": "/sitelogo/www.bestblogs.dev.ico"
-        },
-        {
-          "id": "site-1753279757336",
-          "name": "热榜",
-          "url": "https://trend.tgmeng.com/",
-          "description": "",
-          "icon": ""
         }
       ]
     }
